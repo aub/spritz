1de5dbe764c149e0d95cfdab4a55e5267a3217f8
Aubrey Holland <aubreyholland@gmail.com>
2008-04-15 11:11:22 +0200

    Link to sections by their names.

--- vendor/plugins/spritz_section_links/app/models/link_section.rb
+++ vendor/plugins/spritz_section_links/app/models/link_section.rb
@@ -6,7 +6,7 @@ class LinkSection < Section
   has_many :links, :foreign_key => 'section_id'
 
   def to_url
-    ['link_sections', self.id]
+    [self.name]
   end
   
   def handle_request(request)
