0f874c64230ec59c15143da3acecb34f84181a16
Aubrey Holland <aubreyholland@gmail.com>
2008-04-18 11:19:27 +0200

    Add flash display, include lowpro, and add ability to delete sections.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,43 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Event.onReady(function() {
+  ['notice', 'errors'].each(function(flashType) {
+    var elem = $('flash-' + flashType);
+    if (elem.innerHTML != '') Flash.show(flashType, elem.innerHTML);
+  })  
+});
+
+/*-------------------- Flash ------------------------------*/
+// Flash is used to manage error messages and notices from 
+// Ajax calls.
+//
+var Flash = {
+  // When given an flash message, wrap it in a list 
+  // and show it on the screen.  This message will auto-hide 
+  // after a specified amount of milliseconds
+  show: function(flashType, message) {
+    new Effect.ScrollTo('flash-' + flashType);
+    $('flash-' + flashType).innerHTML = '';
+    if (message.toString().match(/<li/)) message = "<ul>" + message + '</ul>'
+    $('flash-' + flashType).innerHTML = message;
+    new Effect.Appear('flash-' + flashType, {duration: 0.3});
+    setTimeout(Flash['fade' + flashType[0].toUpperCase() + flashType.slice(1, flashType.length)].bind(this), 5000)
+  },
+  
+  errors: function(message) {
+    this.show('errors', message);
+  },
+
+  // Notice-level messages.  See Messenger.error for full details.
+  notice: function(message) {
+    this.show('notice', message);
+  },
+  
+  // Responsible for fading notices level messages in the dom    
+  fadeNotice: function() {
+    new Effect.Fade('flash-notice', {duration: 0.3});
+  },
+  
+  // Responsible for fading error messages in the DOM
+  fadeError: function() {
+    new Effect.Fade('flash-errors', {duration: 0.3});
+  }
+}
--- vendor/plugins/spritz_section_portfolio/app/controllers/admin/portfolio_sections_controller.rb
+++ vendor/plugins/spritz_section_portfolio/app/controllers/admin/portfolio_sections_controller.rb
@@ -3,4 +3,17 @@ class Admin::PortfolioSectionsController < Admin::AdminController
     @section = @site.sections.find(params[:id])
     @portfolio_page = @section.portfolio
   end
+  
+  # DELETE /admin/sites/1
+  # DELETE /admin/sites/1.xml
+  def destroy
+    @section = @site.sections.find(params[:id])
+    @section.destroy
+
+    respond_to do |format|
+      flash[:notice] = "#{@section.title} was successfully destroyed."
+      format.html { redirect_to(admin_sections_path) }
+      format.xml  { head :ok }
+    end
+  end
 end
