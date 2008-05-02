2ccf5ff4c1d520276f3006ac85165a11e35daa94
Aubrey Holland <aubreyholland@gmail.com>
2008-05-02 08:19:28 +0200

    Split a bunch of the asset code into an acts_as_asset extension to allow sharing of the code between other asset types.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -6,41 +6,13 @@ class Asset < ActiveRecord::Base
                  :thumbnails => { :display => '600x400>', :thumb => '70x70!', :tiny => '35x35!' }, 
                  :max_size => 30.megabytes, 
                  :processor => (Object.const_defined?(:ASSET_IMAGE_PROCESSOR) ? ASSET_IMAGE_PROCESSOR : nil)
+
+  # This has to be after has_attachment. It sets up the standard file locations, etc. for all
+  # kinds of assets.
+  acts_as_asset
+  
     
   validates_as_attachment
   validates_presence_of :site_id
-  validate :rename_unique_filename
-  before_validation_on_create :set_site_from_parent
-    
-  protected
-  
-  # When a thumbnail is created, it needs to be given the site id from the image that contains it.
-  def set_site_from_parent
-    self.site_id = parent.site_id if parent_id
-  end
-
-  # Override the full_filename method from attachment_fu in order to have more control over the
-  # location of the files. In this case, they are stored with a configurable root path, then the
-  # site's subdomain, and then the date.
-  def full_filename(thumbnail=nil)
-    date = self.created_at || Time.now
-    File.join(RAILS_ROOT, ASSET_PATH_ROOT, self.site.subdomain, 
-              date.year.to_s, date.month.to_s, date.day.to_s, 
-              thumbnail_name_for(thumbnail))    
-  end
-
-  # When an asset it saved, we need to make sure that there isn't already a file with that name in the
-  # same directory. If there is, then append an integer to the end until we find a free file name.
-  def rename_unique_filename
-    if ((@old_filename && !@old_filename.eql?(full_filename)) || new_record?) && errors.empty? && site_id && filename
-      i      = 1
-      pieces = filename.split('.')
-      ext    = pieces.size == 1 ? nil : pieces.pop
-      base   = pieces * '.'
-      while File.exists?(full_filename)
-        write_attribute :filename, base + "_#{i}#{".#{ext}" if ext}"
-        i += 1
-      end
-    end
-  end
+        
 end
--- config/initializers/spritz_init.rb
+++ config/initializers/spritz_init.rb
@@ -1,1 +1,4 @@
 require 'liquid/initializer'
+require 'acts_as_asset'
+
+ActiveRecord::Base.send(:extend, ActsAsAsset)
