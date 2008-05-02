193c9baf8a554f8f7db5b09e5e0ec0f6c1ce6615
Aubrey Holland <aubreyholland@gmail.com>
2008-05-02 12:09:23 +0200

    Drop the sections table and remove the settings column from sites.

--- db/migrate/011_add_setting_fields_to_site.rb
+++ db/migrate/011_add_setting_fields_to_site.rb
@@ -2,10 +2,12 @@ class AddSettingFieldsToSite < ActiveRecord::Migration
   def self.up
     add_column :sites, :theme, :string
     add_column :sites, :title, :string
+    remove_column :sites, :settings
   end
 
   def self.down
     remove_column :sites, :theme
     remove_column :sites, :title
+    add_column :sites, :settings, :text
   end
 end
