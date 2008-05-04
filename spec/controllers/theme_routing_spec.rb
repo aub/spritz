fbfe750960d5a5bab7ffd3dd8346fcb0c495be26
Aubrey Holland <aubreyholland@gmail.com>
2008-05-04 08:28:33 +0200

    Change the theme routes from /stylesheets/theme to /theme/stylesheets, etc.

--- spec/controllers/theme_routing_spec.rb
+++ spec/controllers/theme_routing_spec.rb
@@ -3,31 +3,31 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe ThemeController do
   describe "route generation" do
 
-    it "should map { :controller => 'theme', :action => 'stylesheets', :filename => 'f', :ext => 'css' } to /stylesheets/theme/f.css" do
-      route_for(:controller => "theme", :action => "stylesheets", :filename => 'f', :ext => 'css').should == "/stylesheets/theme/f.css"
+    it "should map { :controller => 'theme', :action => 'stylesheets', :filename => 'f', :ext => 'css' } to /theme/stylesheets/f.css" do
+      route_for(:controller => "theme", :action => "stylesheets", :filename => 'f', :ext => 'css').should == "/theme/stylesheets/f.css"
     end
     
-    it "should map { :controller => 'theme', :action => 'images', :filename => 'f', :ext => 'png' } to /images/theme/f.png" do
-      route_for(:controller => "theme", :action => "images", :filename => 'f', :ext => 'png').should == "/images/theme/f.png"
+    it "should map { :controller => 'theme', :action => 'images', :filename => 'f', :ext => 'png' } to /theme/images/f.png" do
+      route_for(:controller => "theme", :action => "images", :filename => 'f', :ext => 'png').should == "/theme/images/f.png"
     end
     
-    it "should map { :controller => 'theme', :action => 'javascripts', :filename => 'f', :ext => 'js' } to /javascripts/theme/f.js" do
-      route_for(:controller => "theme", :action => "javascripts", :filename => 'f', :ext => 'js').should == "/javascripts/theme/f.js"
+    it "should map { :controller => 'theme', :action => 'javascripts', :filename => 'f', :ext => 'js' } to /theme/javascripts/f.js" do
+      route_for(:controller => "theme", :action => "javascripts", :filename => 'f', :ext => 'js').should == "/theme/javascripts/f.js"
     end
   end
 
   describe "route recognition" do
 
-    it "should generate params { :controller => 'theme', action => 'stylesheets', :filename => 'f', :ext => 'css' } from GET /stylesheets/theme/f.css" do
-      params_from(:get, "/stylesheets/theme/f.css").should == {:controller => "theme", :action => "stylesheets", :filename => 'f', :ext => 'css'}
+    it "should generate params { :controller => 'theme', action => 'stylesheets', :filename => 'f', :ext => 'css' } from GET /theme/stylesheets/f.css" do
+      params_from(:get, "/theme/stylesheets/f.css").should == {:controller => "theme", :action => "stylesheets", :filename => 'f', :ext => 'css'}
     end
     
-    it "should generate params { :controller => 'theme', action => 'images', :filename => 'f', :ext => 'png' } from GET /images/theme/f.png" do
-      params_from(:get, "/images/theme/f.png").should == {:controller => "theme", :action => "images", :filename => 'f', :ext => 'png'}
+    it "should generate params { :controller => 'theme', action => 'images', :filename => 'f', :ext => 'png' } from GET /theme/images/f.png" do
+      params_from(:get, "/theme/images/f.png").should == {:controller => "theme", :action => "images", :filename => 'f', :ext => 'png'}
     end
     
-    it "should generate params { :controller => 'theme', action => 'javascripts', :filename => 'f', :ext => 'js' } from GET /javascripts/theme/f.js" do
-      params_from(:get, "/javascripts/theme/f.js").should == {:controller => "theme", :action => "javascripts", :filename => 'f', :ext => 'js'}
+    it "should generate params { :controller => 'theme', action => 'javascripts', :filename => 'f', :ext => 'js' } from GET /theme/javascripts/f.js" do
+      params_from(:get, "/theme/javascripts/f.js").should == {:controller => "theme", :action => "javascripts", :filename => 'f', :ext => 'js'}
     end
   end
 end
