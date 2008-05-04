f7fdfe13cbd0ad99cd207f8e729513d3ed00a789
Aubrey Holland <aubreyholland@gmail.com>
2008-05-04 08:27:19 +0200

    Add url filters for linking to stylesheets and javascript files.

--- lib/liquid/url_filters.rb
+++ lib/liquid/url_filters.rb
@@ -2,10 +2,15 @@ module UrlFilters
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::AssetTagHelper
   
-  # def link_to_asset(asset, *args)
-  #   text = h(args[0]) || image_tag(asset.thumbnail_path)
-  #   content_tag :a, text, { :href => asset['url'] }
-  # end
+  def stylesheet(stylesheet, media = nil)
+    stylesheet << '.css' unless stylesheet.include? '.'
+    tag 'link', :rel => 'stylesheet', :type => 'text/css', :href => "theme/stylesheets/#{stylesheet}", :media => media
+  end
+
+  def javascript(javascript)
+    javascript << '.js' unless javascript.include? '.'
+    content_tag 'script', '', :type => 'text/javascript', :src => "theme/javascripts/#{javascript}"
+  end
     
 end
 
