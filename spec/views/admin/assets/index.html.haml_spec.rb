a1e735dae88b33302aa8ea636c812c42bf657dbf
Aubrey Holland <aubreyholland@gmail.com>
2008-05-07 15:18:37 +0200

    Fix failing specs.

--- spec/views/admin/assets/index.html.haml_spec.rb
+++ spec/views/admin/assets/index.html.haml_spec.rb
@@ -2,20 +2,20 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 
 describe "/admin/assets/index.html.haml" do
   include Admin::AssetsHelper
+
+  define_models :index do
+    model Asset do
+      stub :one, :site => all_stubs(:site), :filename => 'fake'
+    end
+  end
   
   before(:each) do
-    @asset_98 = mock_model(Asset)
-    @asset_98.should_receive(:public_filename).and_return("MyString")
-    @asset_99 = mock_model(Asset)
-    @asset_99.should_receive(:public_filename).and_return("MyString")
-
-    assigns[:assets] = [@asset_98, @asset_99]
+    assigns[:assets] = Asset.paginate :page => 1, :per_page => 18
   end
 
   it "should render list of admin_assets" do
     render "/admin/assets/index.html.haml"
-    response.should have_tag("a[href=?]", edit_admin_asset_path(@asset_98))
-    response.should have_tag("a[href=?]", edit_admin_asset_path(@asset_99))
+    response.should have_tag("a[href=?]", edit_admin_asset_path(assets(:one)))
   end
 end
 
--- spec/views/admin/sites/new.html.haml_spec.rb
+++ spec/views/admin/sites/new.html.haml_spec.rb
@@ -10,14 +10,19 @@ describe "/admin/sites/new.html.erb" do
     @site.stub!(:subdomain).and_return("MyString")
     @site.stub!(:title).and_return('title')
     assigns[:template_site] = @site
+    
+    @user = mock_model(User)
+    @user.stub!(:login).and_return('a')
+    @user.stub!(:email).and_return('a')
+    @user.stub!(:password).and_return('a')
+    @user.stub!(:password_confirmation).and_return('a')
+    assigns[:user] = @user
   end
 
   it "should render new form" do
     render "/admin/sites/new.html.haml"
     
     response.should have_tag("form[action=?][method=post]", admin_sites_path) do
-      with_tag("input#site_domain[name=?]", "site[domain]")
-      with_tag("input#site_subdomain[name=?]", "site[subdomain]")
     end
   end
 end
